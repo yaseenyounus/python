c32c16bd759f7036c73c8c8a886ed07d4691e10f
Yaseen Younus <yay200@gmail.com>
2019-05-12 13:03:18 -0500

    Adding the LargestNumList.py file

---
+++ LargestNumList.py
@@ -0,0 +1,17 @@
+import random
+
+numList = []
+max = numList[0]
+
+# Inserts random numbers into list ranging from 1 to 100
+for x in range(100):
+    numList.append(random.randrange(1, 101))
+
+# Loops through the list and compares every element to the max to find a new max
+for x in numList:
+    if x > max:
+        max = x
+
+print(max)
+
+
