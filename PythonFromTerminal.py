31fdd7891f29b143c952e202915ffde2de778bb2
Yaseen Younus <yay200@gmail.com>
2019-07-28 17:51:45 -0500

    Adding new Python program where I only used the terminal to edit and push the file. Testing out the terminal-only life

---
+++ PythonFromTerminal.py
@@ -0,0 +1,5 @@
+# Having some fun with the terminal, GIT and vim. Created this file using the tocuh command, then will push it to my Python repo
+
+print("Having some fun with Python from the VIM editor in my terminal")
+
+
