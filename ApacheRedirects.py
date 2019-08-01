131bb1d5824c3fc5ad08b1afaa650b2a19114dea
Yaseen Younus <yay200@gmail.com>
2019-07-31 21:03:50 -0500

    Adding Python program that generates Apache rewrite rules

---
+++ ApacheRedirects.py
@@ -0,0 +1,29 @@
+# Yaseen Younus
+# 31 July 2019
+# This program generates Apache Rewrite rules based on the source and destination sites provided to the function. It works with internal as well as external redirects.
+
+# RewriteRule ^/source-path$ https://destination-url-for-external-redirect.html [R=301,NC,L]
+# RewriteRule ^/source-path$ /destination-path-for-internal-redirect.html [R=301,NC,L]
+# Example : Redirect www.abbott.com/news to www.abbott.com/newsroom.html
+
+import sys 
+
+def redirect_to_internal(path_from, path_to):
+    print("RewriteRule ^{}$ {} [R=301,NC,L]".format(removeRoot(path_from), removeRoot(path_to)))
+
+def redirect_to_external(path_from, path_to):
+    print("RewriteRule ^{}$ {} [R=301,NC,L]".format(removeRoot(path_from), path_to))
+
+def removeRoot(url):
+    if url[:url.find('/') == -1]:
+        return '/'
+    else:
+        return url[url.find('/'):]
+
+
+url_from, url_to = sys.argv[1], sys.argv[2]
+
+if url_from[:url_from.find('/')] == url_to[:url_to.find('/')]:
+    redirect_to_internal(url_from, url_to)
+else:
+    redirect_to_external(url_from, url_to)
